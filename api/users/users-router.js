d6b279d734c19da9ab4cd4a744e7c8345633559c
Camden Hess <camden.hess12@gmail.com>
2024-03-27 16:54:19 -0600

    added put functionality

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -39,12 +39,18 @@ router.post('/', validateUser, (req, res, next) => {
     .catch(next)
 });
 
-router.put('/:id', validateUserId, validateUser, (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
-  console.log(req.user)
-  console.log(req.name)
+  User.update(req.params.id, { name: req.name })
+    .then(() => {
+      return User.getById(req.params.id)
+    })
+    .then(newUser => {
+      res.json(newUser)
+    })
+    .catch(next)
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
